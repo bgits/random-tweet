e4773d29ff65bbb21c6e85ef483ecb4c6dccdd6f
Barry Gitarts <bgitarts@gmail.com>
2016-03-01 15:08:48 -0500

    add OAuth1Session

--- random_tweet.py
+++ random_tweet.py
@@ -6,3 +6,12 @@ from requests_oauthlib import OAuth1Session
 
 class TwitterApi(object):
     BASE_URL = 'https://api.twitter.com/1.1/search/tweets.json'
+
+    def __init__(self, consumer_key,
+                 consumer_secret,
+                 access_token,
+                 token_secret):
+        self._session = OAuth1Session(consumer_key,
+                                      consumer_secret,
+                                      access_token,
+                                      token_secret)
